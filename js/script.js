2a703dc606d5f5e0866cf13bfc339abd8d571f5e
SKanae-afk <sebyghenciu5@gmail.com>
2023-12-09 19:38:35 +0200

    menu working - login not working

--- js/script.js
+++ js/script.js
@@ -1,18 +1,31 @@
+let body = document.body;
 let profile = document.querySelector('.header .flex .profile');
+let searchForm = document.querySelector('.header .flex .search-form'); // Define searchForm here
+let sideBar = document.querySelector('.side-bar');
 
 document.querySelector('#user-btn').onclick = () => {
   profile.classList.toggle('active');
-  searchForm.classList.remove('active');
+  searchForm.classList.toggle('active');
 };
-
-let searchForm = document.querySelector('.header .flex .search-form');
-
 document.querySelector('#search-btn').onclick = () => {
   searchForm.classList.toggle('active');
   profile.classList.remove('active');
+};
+document.querySelector('#menu-btn').onclick = () => {
+  sideBar.classList.toggle('active');
+  body.classList.toggle('active');
+};
+document.querySelector('.side-bar .close-side-bar').onclick = () => {
+  sideBar.classList.remove('active');
+  body.classList.remove('active');
 };
 
 window.onscroll = () => {
   profile.classList.remove('active');
   searchForm.classList.remove('active');
+
+  if (window.innerWidth < 1200) {
+    sideBar.classList.remove('active');
+    body.classList.remove('active');
+  }
 };
